ee3201af668a8f2190834a60034628a8a1459f27
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-03-28 17:51:46 +0100

    RTL docker added. not yet working.

---
+++ testRTLRaw.sh
@@ -0,0 +1,15 @@
+export NETWORK="mainnet"
+export RPCUSER="bongo" && \
+export BTC_RPCUSER="bongo" && \
+export RPCPASS="longo" && \
+export BTC_RPCPASSWORD="longo" &&
+export NETWORK="mainnet" 
+
+
+docker run \
+   --env 'BTC_RPCUSER=$BTC_RPCUSER' \
+   --env 'BTC_RPCPASSWORD=$BTC_RPCPASSWORD' \
+   --env 'BTC_TXINDEX=1' \
+   --volume /datadrive/lnd:/lnd \
+   -t -i  rtl \
+   /bin/sh

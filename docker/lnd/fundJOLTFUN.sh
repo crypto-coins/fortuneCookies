2657e0a41503eb7fc848b9d04bd0969e65bb1d0f
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-03-31 16:51:14 +0200

    bugfixes

--- docker/lnd/fundJOLTFUN.sh
+++
@@ -1,8 +0,0 @@
-
-
-lncli --rpcserver localhost:10001 \
-      --macaroonpath /root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon \
-      --network mainnet --chain bitcoin \
-      openchannel 0374ecf61ed6c1208c42339f47decde2bc0c4393ac95f07827b3471e939d7eb961 100000
-
-      

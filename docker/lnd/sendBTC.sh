7de5726f56f87f4ca5845758c0dac85c09d863ef
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-04-12 13:55:27 +0200

    send btc outgoing added

--- docker/lnd/sendBTC.sh
+++ docker/lnd/sendBTC.sh
@@ -1,11 +1,6 @@
 
 
-# https://joltfun.com/#page/Lightning
-
 lncli --rpcserver localhost:10001 \
       --macaroonpath /root/.lnd/data/chain/bitcoin/mainnet/admin.macaroon \
       --network mainnet --chain bitcoin \
-      closechannel  \
-      6172de1edc4adc074ee1a4d42cef70c85cceb183a1b9d28cea3a2d16409deea9 0
-
-      
+      sendcoins 38kC7ho4RzmUQX41djYsnSxevKmLBTZFtU  2913878 
